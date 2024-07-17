f9529723235af8ce9bb877b81730b2753bd39f81
Oksana Shik <shikoksana57@gmail.com>
2024-07-17 19:36:41 +0300

    add datasource file

---
+++ ServletCRUDService/src/main/java/org/crudservice/util/DataSource.java
@@ -0,0 +1,35 @@
+package org.crudservice.util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource {
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/message_user");
+        config.setUsername("root");
+        config.setPassword("12345678");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.addDataSourceProperty("useSSL", false);
+        ds = new HikariDataSource(config);
+    }
+
+    private DataSource() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
